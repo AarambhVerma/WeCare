f60e3106c4ff7f3b4954b6231058701c8739d66c
Aarambh Verma <aarambhverma246@gmail.com>
2021-05-28 15:01:39 +0530

    change in views file structure

--- routes/index.js
+++ routes/index.js
@@ -2,52 +2,16 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/authcheck')
 
-
-
-//template engine route
+// @desc    Show Index Page
+// @route   GET /
 router.get("/",(req,res) => {
     res.render('index',{
         user : req.user
     })
 })
 
-
-router.get("/about-us",(req,res) =>{
-    res.render('about_us',{
-        user : req.user
-    })
-})
-
-router.get("/blog",(req,res) =>{
-    res.render('blog',{
-        user : req.user
-    })
-})
-
-router.get("/contact-us",(req,res) =>{
-    res.render('contact_us',{
-        user : req.user
-    })
-})
-
-router.get("/gallery",(req,res) =>{
-    res.render('gallery',{
-        user : req.user
-    })
-})
-
-router.get("/profile", ensureAuth, (req,res) =>{
-    res.render('profile',{
-        user : req.user
-    })
-})
-
-router.get("/services",(req,res) =>{
-    res.render('services',{
-        user : req.user
-    })
-})
-
+// @desc    Show User Dashboard
+// @route   GET /
 router.get("/dashboard", ensureAuth, (req,res) =>{
     res.render('dashboard', {
         // user : req.user, // TODO navbar conflict 
@@ -55,6 +19,5 @@ router.get("/dashboard", ensureAuth, (req,res) =>{
         isAdmin: req.user.isAdmin
     })
 })
-
 
 module.exports = router
